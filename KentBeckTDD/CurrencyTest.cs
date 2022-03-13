7e770651613e326c1d3a3daea8609fbfe41a0a49
Cassie <cassie.nieh@568win.com>
2022-03-13 16:02:38 +0800

    [Chapter 1] Init testMultiplication

---
+++ KentBeckTDD/CurrencyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KentBeckTDD
+{
+    class CurrencyTest
+    {
+        public void testMultiplicstion()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            assertEquals(10, five.amount);
+        }
+    }
+}
