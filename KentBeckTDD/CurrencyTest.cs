dc9d54084c6be4d8f329dcd37c90458bb7c5d574
Cassie <cassie.nieh@568win.com>
2022-03-13 16:40:17 +0800

    [Chapter 1] Run Test and Get red light

--- KentBeckTDD/CurrencyTest.cs
+++ KentBeckTDD/CurrencyTest.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace KentBeckTDD
 {
-    class CurrencyTest
+    [TestFixture]
+    public class CurrencyTest
     {
+        [Test]
         public void testMultiplicstion()
         {
             Dollar five = new Dollar(5);
             five.times(2);
-            assertEquals(10, five.amount);
+            Assert.Equals(10, five.amount);
         }
 
         public class Dollar { 
             public Dollar(int amount) { }
+            public void times(int multiplier)
+            {
+
+            }
+            public int amount;
         }
 
     }
