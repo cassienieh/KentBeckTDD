f17b26f009f9c9d6da08cc52b45069088101e616
Cassie <cassie.nieh@568win.com>
2022-03-13 17:04:40 +0800

    [Chapter 2] Add a new test case

--- KentBeckTDD/CurrencyTest.cs
+++ KentBeckTDD/CurrencyTest.cs
@@ -14,6 +14,8 @@ namespace KentBeckTDD
             Dollar five = new Dollar(5);
             five.times(2);
             Assert.AreEqual(10, five.amount);
+            five.times(3);
+            Assert.AreEqual(15, five.amount);
         }
        
     }
