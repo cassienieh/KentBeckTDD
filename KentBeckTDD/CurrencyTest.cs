499003e518286ffefa436c0482fe69755e4bd8dc
Cassie <cassie.nieh@568win.com>
2022-03-24 01:38:06 +0800

    [Chapter 4] Compare with two Dollar object

--- KentBeckTDD/CurrencyTest.cs
+++ KentBeckTDD/CurrencyTest.cs
@@ -13,9 +13,9 @@ namespace KentBeckTDD
         {
             Dollar five = new Dollar(5);
             Dollar product = five.times(2);
-            Assert.AreEqual(10, product.amount);
+            Assert.AreEqual(new Dollar(10), product);
             product = five.times(3);
-            Assert.AreEqual(15, product.amount);
+            Assert.AreEqual(new Dollar(15), product);
         }
 
         [Test]
