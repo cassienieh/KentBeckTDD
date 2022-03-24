3376e9f622bb9776d52423fd52b32acbd30ec39c
Cassie <cassie.nieh@568win.com>
2022-03-25 02:29:14 +0800

    [Chapter 6] Franc inherit Money

--- KentBeckTDD/CurrencyTest.cs
+++ KentBeckTDD/CurrencyTest.cs
@@ -28,8 +28,10 @@ namespace KentBeckTDD
         [Test]
         public void testEquality()
         {
-            Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
-            Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+            Assert.True(new Dollar(5).Equals(new Dollar(5)));
+            Assert.False(new Dollar(5).Equals(new Dollar(6)));
+            Assert.True(new Franc(5).Equals(new Franc(5)));
+            Assert.False(new Franc(5).Equals(new Franc(6)));
         }
        
     } 
--- KentBeckTDD/Franc.cs
+++ KentBeckTDD/Franc.cs
@@ -4,22 +4,21 @@ using System.Text;
 
 namespace KentBeckTDD
 {
-    public class Franc
+    public class Franc : Money
     {
-        private int amount;
         public Franc(int amount)
         {
-            this.amount = amount;
+            this.Amount = amount;
         }
         public Franc times(int multiplier)
         {
-            return new Franc(amount * multiplier);
+            return new Franc(Amount * multiplier);
         }
 
         public override bool Equals(Object obj)
         {
-            Franc dollar = (Franc)obj;
-            return amount == dollar.amount;
+            Money money = (Money)obj;
+            return Amount == money.GetAmount();
         }
 
     }
